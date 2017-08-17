135ee934d100c269547ba901854643d16d1a1aa7
徐志飞 <wb-xzf247027@alibaba-inc.com>
2017-08-17 22:02:15 +0800

    构建项目

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -3,50 +3,56 @@ import { render } from 'react-dom'
 import { Router, Route, Link, hashHistory } from 'react-router';
 
 const App = React.createClass({
-  render() {
-    return (
-      <div>
-        <h1>App</h1>
-        <ul>
-          <li><Link to="/about">About</Link></li>
-          <li><Link to="/inbox">Inbox</Link></li>
-        </ul>
-        {this.props.children}
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div>
+                <h1>App</h1>
+                <ul>
+                    <li><Link to="/about">About</Link></li>
+                    <li><Link to="/inbox">Inbox</Link></li>
+                </ul>
+                {this.props.children}
+            </div>
+        )
+    }
 })
 
 const About = React.createClass({
-  render() {
-    return <h3>About</h3>
-  }
+    render() {
+        return <h3>About</h3>
+    }
 })
 
 const Inbox = React.createClass({
-  render() {
-    return (
-      <div>
-        <h2>Inbox</h2>
-        {this.props.children || "Welcome to your Inbox"}
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div>
+                <h2>Inbox</h2>
+                {this.props.children || "Welcome to your Inbox"}
+            </div>
+        )
+    }
 })
 
 const Message = React.createClass({
-  render() {
-    return <h3>Message {this.props.params.id}</h3>
-  }
+    render() {
+        return <h3>Message {this.props.params.id}</h3>
+    }
 })
 
 render((
-  <Router history={hashHistory}>
-    <Route path="/" component={App}>
-      <Route path="about" component={About} />
-      <Route path="inbox" component={Inbox}>
-        <Route path="messages/:id" component={Message} />
-      </Route>
-    </Route>
-  </Router>
+    <Router history={hashHistory}>
+        <Route path="/" component={App}>
+            <Route path="about" component={About} />
+            <Route path="inbox" component={Inbox}>
+                <Route path="messages/:id" component={Message} />
+            </Route>
+        </Route>
+        <Route path="/index" component={App}>
+            <Route path="/about" component={About} />
+            <Route path="/inbox" component={Inbox}>
+                <Route path="messages/:id" component={Message} />
+            </Route>
+        </Route>
+    </Router>
 ), document.body)
